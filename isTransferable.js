a4b307f3ffb38a4a3a7939c9e3ac73839ed69904
kdjn <kdjn6223@gmail.com>
2023-08-14 23:29:58 +0900

    初回コミット

---
+++ isTransferable.js
@@ -0,0 +1,15 @@
+const {MessagePort} = require("worker_threads");
+/**
+ *
+ * @param {any} data
+ * @return {boolean}
+ */
+module.exports = (data)=>
+{
+	return (
+		data instanceof Buffer ||
+		data instanceof ArrayBuffer ||
+		data instanceof MessagePort ||
+		(typeof data === "object" && data !== null && data.constructor.name === "ImageBitmap")
+	)
+}
